3460d9b97a376fe3012391e5dfd78e4d56826499
ThomasLacroix1 <144111645+ThomasLacroix1@users.noreply.github.com>
2024-03-05 09:26:32 +0100

    change

--- _Projet_1_Social-mediia-dashboard-Start/tailwind.config.js
+++ _Projet_1_Social-mediia-dashboard-Start/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        forground:"hsl(var(--forground)/<alpha-value>)",
+        background:"hsl(var(--background)/<alpha-value>)",
+        topBackgroundPattern:"hsl(var(--top-background-pattern)/<alpha-value>)",
+        cardBackground:"hsl(var(--card-background)/<alpha-value>)",
+        cardHoverBackground:"hsl(var(--card-hover-background)/<alpha-value>)",
+        text:"hsl(var(--text)/<alpha-value>)"
+      },
+    },
   },
   plugins: [],
 }
